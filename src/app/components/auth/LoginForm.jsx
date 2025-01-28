b9000380f36f11a4ca2d5ad134dce3db1f49a9c2
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-01-28 19:30:56 +0600

    Add axios for API calls in LoginForm and handle login submission

--- src/app/components/auth/LoginForm.jsx
+++ src/app/components/auth/LoginForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 import Field from "../common/Field";
 import { useRouter } from "next/navigation";
@@ -12,14 +13,35 @@ const LoginForm = () => {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm();
 
   const router = useRouter();
 
-  const onSubmit = (formData) => {
-    const user = { ...formData };
-    setAuth({ user });
-    router.push("/");
+  const onSubmit = async (formData) => {
+    try {
+      console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`,
+        formData
+      );
+      console.log(response.data);
+      if (response?.status === 200) {
+        const { user, token } = response.data;
+        if (token) {
+          const authToken = token.token;
+          const refreshToken = token.refreshToken;
+          setAuth({ user, authToken, refreshToken });
+          router.push("/");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setError("root.random", {
+        type: "random",
+        message: `Invalid email ${formData.email} is Not Found`,
+      });
+    }
   };
 
   return (
@@ -72,6 +94,7 @@ const LoginForm = () => {
       </Field>
 
       {/* Submit Button */}
+      <p>{errors?.root?.random?.message}</p>
       <Field>
         <button
           className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
